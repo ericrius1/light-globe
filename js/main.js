1403197d785ee310434a83ae3929320fc40faead
Eric Levin <ericrius1@gmail.com>
2015-05-07 22:58:43 -0700

    post processing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var scene, camera, renderer, earth, light, controls, stats;
+var scene, camera, renderer, composer, earth, light, controls, stats;
+var renderModel, effectBloom, effectCopy, effectFXAA;
 
 
 var fakeDataServer = new FakeDataServer();
@@ -8,7 +9,7 @@ var randFloat = THREE.Math.randFloat;
 var shaders = new ShaderLoader('shaders');
 shaders.load('earth_vert', 'earth', 'vertex');
 shaders.load('earth_frag', 'earth', 'fragment');
-shaders.shaderSetLoaded = function(){
+shaders.shaderSetLoaded = function() {
   init();
 }
 
@@ -21,6 +22,20 @@ function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  renderer.autoClear = false;
+  renderModel = new THREE.RenderPass(scene, camera);
+  effectBloom = new THREE.BloomPass(0);
+  effectCopy = new THREE.ShaderPass(THREE.CopyShader);
+  effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+  effectFXAA.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+  effectCopy.renderToScreen = true;
+
+  composer = new THREE.EffectComposer(renderer);
+  composer.addPass(renderModel);
+  composer.addPass(effectFXAA);
+  composer.addPass(effectBloom);
+  composer.addPass(effectCopy);
 
   stats = new Stats();
   document.body.appendChild(stats.domElement);
@@ -35,20 +50,25 @@ function init() {
   earth.yehior();
   animate();
 
-  
+
 
 }
 
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  renderer.clear();
+  composer.render();
   controls.update();
   stats.update()
 }
 
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 
 window.addEventListener('resize', onResize, false);
-
-
